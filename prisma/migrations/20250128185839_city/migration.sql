a4f6e644331f0e8d5922fc212e677b2387239283
Rahul Dey <rahuldey832000@gmail.com>
2025-01-29 00:30:08 +0530

    Add city table schema and migration

---
+++ prisma/migrations/20250128185839_city/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Cities" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Cities_code_key" ON "Cities"("code");
